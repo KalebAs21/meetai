45d222002676166bb11138f095c57aaa87434daa
kaleb <kalebasmamaw21@gmail.com>
2026-02-13 08:46:51 +0300

    meetings data table added

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -2,13 +2,15 @@
 import { MeetigsViewLoading, MeetingsView, MeetingsViewError } from '@/modules/meetings/ui/views/meetings-view'
 import React, { Suspense } from 'react'
 import { getQueryClient, trpc } from '@/trpc/server';
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { QueryClient, dehydrate, HydrationBoundary } from '@tanstack/react-query';
 import { ErrorBoundary } from 'react-error-boundary';
 import { MeetingsListHeader } from '@/modules/meetings/ui/components/meetings-list-header';
 
 
+const queryClient = new QueryClient();
+
 function page() {
-  const queryClient = getQueryClient();
+//   const queryClient = getQueryClient();
   void queryClient.prefetchQuery(
     trpc.meetings.getMany.queryOptions({})
   )
